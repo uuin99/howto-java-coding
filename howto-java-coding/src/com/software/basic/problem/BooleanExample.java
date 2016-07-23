1fb8d220dc45f54c9c626ac15e1edf67d5115a4c
uuin99 <uuin99@gmail.com>
2016-07-23 16:23:04 +0900

    1-9. Boolean 객체의 사용법
    BooleanInstantiation

---
+++ howto-java-coding/src/com/software/basic/problem/BooleanExample.java
@@ -0,0 +1,8 @@
+package com.software.basic.problem;
+
+public class BooleanExample {
+	//불필요한 Boolean 객체를 생성한 예
+	public static void main(String[] args) {
+		Boolean bool = new Boolean(true);
+	}
+}
---
+++ howto-java-coding/src/com/software/basic/solution/BooleanExample.java
@@ -0,0 +1,14 @@
+package com.software.basic.solution;
+
+public class BooleanExample {
+	
+	//Boolean 객체를 올바르게 사용하는 예
+	public static void main(String[] args) {
+		
+		//Boolean bool = new Boolean(true);
+		Boolean bool = Boolean.TRUE;
+		
+		//Boolean bool2 = Boolean.valueOf(false);
+		Boolean bool2 = Boolean.FALSE;
+	}
+}
